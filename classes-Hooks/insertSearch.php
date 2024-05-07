65999de286d562fa213dbbc65e7a9312c7cc268a
Heinz <heinz_schweitzer@t-online.de>
2024-05-07 22:47:28 +0200

    does insert into table search while loading files

---
+++ classes-Hooks/insertSearch.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * ***********************************************
+ * fill tabel searches with certain column values
+ * to allow fulltext serach on these values
+ * **********************************************
+ */
+
+class insertSearch {
+
+    private $db, $isbd, $insert;
+
+    function __construct($db) {
+        $this->db = $db;
+        $q = "insert into search (titleid,colname,what) values (?,?,?)";
+        $this->insert = $db->prepare($q);
+        $this->isbd = new isbdElements($db);
+    }
+
+    function insert($titleid) {
+
+        $this->isbd->getAllTags($titleid);
+
+        $ti = $this->isbd->title();
+        $this->insert->execute([$titleid, 'title', $ti]);
+        $ti = $this->isbd->author();
+        $this->insert->execute([$titleid, 'autor', $ti]);
+        $ti = $this->isbd->verlag();
+        $this->insert->execute([$titleid, 'verlag', $ti]);
+    }
+}
