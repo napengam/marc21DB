9025b5a8f5a5cc78e2803d979839c3cff0ea0fd0
Heinz <heinz_schweitzer@t-online.de>
2025-12-12 10:04:58 +0100

    file_get_contents had problems with openssl etc

--- classes-GUI/showBookContent.php
+++ classes-GUI/showBookContent.php
@@ -16,8 +16,7 @@ class showBookContent {
          * create page
          * **********************************************
          */
-
-        $out = file_get_contents($href);
+        $out = $this->fetch_url($href);
 
         if (strpos($out, '<html>')) {
             echo $out;
@@ -34,6 +33,29 @@ class showBookContent {
         $pg->closeContainer();
         $pg->closeSection();
         $pg->closePage();
+    }
+
+    function fetch_url($url) {
+        $ch = curl_init($url);
+
+        curl_setopt_array($ch, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_SSL_VERIFYPEER => false, // disable cert verification
+            CURLOPT_SSL_VERIFYHOST => false, // disable hostname check
+            CURLOPT_USERAGENT => "Mozilla/5.0",
+            CURLOPT_CONNECTTIMEOUT => 10,
+            CURLOPT_TIMEOUT => 30,
+        ]);
+
+        $data = curl_exec($ch);
+
+        if ($data === false) {
+            throw new Exception("cURL error: " . curl_error($ch));
+        }
+
+        curl_close($ch);
+        return $data;
     }
 }
 
